8b30118a5cc79a9185e408aee9c3addbb7033f6e
HanJian <jhohanjianpierre@gmail.com>
2022-12-29 20:14:50 -0300

    3era clase

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,12 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+  for(let i = 0; i < startEl.children.length; i++){
+    let result = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+    resultSet = [...resultSet, ...result];
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +23,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  if(selector[0] === '.') return 'class';
+  if(selector.includes('.')) return 'tag.class';
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +38,28 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = (elemento) => `#${elemento.id}` === selector;
   } else if (selectorType === "class") {
-    
+    matchFunction = (elemento) => { 
+      for(let i = 0; i < elemento.classList.length; i++){
+        if(`.${elemento.classList[i]}` === selector) return true;
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (elemento) => {
+      // let ele = elemento.classList;
+      // for(let i = 0; i < ele.length; i++){
+      //   if(`${elemento.tagName.toLowerCase()}.${ele[i]}` === selector) return true;
+      // }
+      // return false;
+      let [tag, c] = selector.split('.');
+      let function_tag = matchFunctionMaker(tag);
+      let function_c = matchFunctionMaker('.'+c);
+      return function_tag(elemento) && function_c(elemento);
+    };
   } else if (selectorType === "tag") {
-    
+    matchFunction = (elemento) => elemento.tagName === selector.toUpperCase();
   }
   return matchFunction;
 };
