80eabe110b654a540e45e564320f15748e879d28
HanJian <jhohanjianpierre@gmail.com>
2023-01-02 21:51:13 -0300

    repaso fast 02/01/23

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Botones from "./Botones.jsx";
 
-const studentName = "";
+const studentName = "Han";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>Mi segundo Componente</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((ele, indice)=>(<li key={indice}>{ele}</li>))}
+      </ul>
+      <Botones alerts={alerts}/>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
